ad545d0a1e3770b7b96b867f1605fe7532ee4e82
Usuario <Usuario@B6-603-37795>
2025-03-07 13:35:13 -0500

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MULTIPLICAR
+{
+    public class Class1
+    {
+        public static long multiplicacion(long a, long b)
+        {
+            return (a * b);
+        }
+    }
+}
